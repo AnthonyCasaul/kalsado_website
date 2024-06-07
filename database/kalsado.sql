d9ce1764ec999c2021953dad8a50b78679dc8209
AnthonyCasaul <96120316+AnthonyCasaul@users.noreply.github.com>
2024-06-08 01:53:26 +0800

    update

---
+++ database/kalsado.sql
@@ -0,0 +1,290 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 07, 2024 at 07:52 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kalsado`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brand`
+--
+
+CREATE TABLE `brand` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `brand`
+--
+
+INSERT INTO `brand` (`id`, `name`) VALUES
+(1, 'Nike'),
+(2, 'Puma'),
+(3, 'Adidas'),
+(4, 'Jordan');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `CartID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL COMMENT 'fk_UserID',
+  `ProductID` int(11) NOT NULL COMMENT 'fk_ProductID',
+  `Size` int(11) NOT NULL,
+  `TotalPrice` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`CartID`, `UserID`, `ProductID`, `Size`, `TotalPrice`) VALUES
+(1, 2, 1, 12, 0.00),
+(2, 2, 1, 8, 0.00),
+(3, 2, 1, 11, 0.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `OrderID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL COMMENT 'fk_UserID',
+  `OrderDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `OrderStatus` varchar(10) NOT NULL,
+  `TotalAmount` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `OrderItemsID` int(11) NOT NULL,
+  `OrderID` int(10) NOT NULL COMMENT 'fk_OrderID',
+  `ProductID` int(12) NOT NULL COMMENT 'fk_ProductID',
+  `Quantity` int(11) NOT NULL,
+  `Price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(250) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `category` varchar(50) NOT NULL,
+  `brand` int(10) NOT NULL DEFAULT 1,
+  `quantity_stock` int(100) NOT NULL DEFAULT 1,
+  `description` varchar(10000) NOT NULL,
+  `size` varchar(50) NOT NULL,
+  `product_image` varchar(1000) NOT NULL,
+  `date_created` date NOT NULL DEFAULT current_timestamp(),
+  `date_updated` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `price`, `category`, `brand`, `quantity_stock`, `description`, `size`, `product_image`, `date_created`, `date_updated`) VALUES
+(2, 'try', 1.00, 'male', 1, 2, 'try', '2', '8dbdb3cc40607f5be46eba9ebcfc9041.jpg', '2024-06-08', '2024-06-08'),
+(3, 'Nike', 10000.00, 'male', 3, 1, 'try', '45', 'wallpaperflare.com_wallpaper.jpg', '2024-06-08', '2024-06-08');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
+CREATE TABLE `profile` (
+  `UserID` int(11) NOT NULL,
+  `Username` varchar(50) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Address` varchar(200) NOT NULL,
+  `Fullname` varchar(100) NOT NULL,
+  `PhoneNumber` varchar(50) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  `DoB` date NOT NULL,
+  `Gender` varchar(15) NOT NULL,
+  `Socials` varchar(10) NOT NULL,
+  `role` int(1) NOT NULL DEFAULT 2 COMMENT '1=admin \r\n2=user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `profile`
+--
+
+INSERT INTO `profile` (`UserID`, `Username`, `Email`, `Address`, `Fullname`, `PhoneNumber`, `Password`, `DoB`, `Gender`, `Socials`, `role`) VALUES
+(3, 'anthonycasaul01', 'anthonycasaul17@gmail.com', 'Makati City', 'anthony casaul', '09167412685', 'ed7bcbcb93cbc0ca804053de91a4fd7c', '2024-06-07', 'male', 'Facebook', 2),
+(4, 'admin', 'admin@gmail.com', 'Makati City', 'admin admin', '09167412685', '764e4ae171de8ff97e0f3cff1c6d4665', '2024-06-07', 'male', 'Facebook', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `ReviewID` int(10) NOT NULL,
+  `UserID` int(15) NOT NULL COMMENT 'fk_UserID',
+  `ProductID` int(11) NOT NULL COMMENT 'fk_ProductID',
+  `Rating` int(20) NOT NULL,
+  `ReviewDate` date NOT NULL DEFAULT current_timestamp(),
+  `ReviewText` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `TransactionID` int(11) NOT NULL,
+  `OrderID` int(11) NOT NULL COMMENT 'fk_OrderID',
+  `PaymentMethod` varchar(50) NOT NULL,
+  `PaymentStatus` varchar(10) NOT NULL,
+  `PaymentDate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `PaymentAmount` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `brand`
+--
+ALTER TABLE `brand`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`CartID`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`OrderID`);
+
+--
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`OrderItemsID`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`UserID`),
+  ADD UNIQUE KEY `Username` (`Username`,`Email`);
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`ReviewID`);
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`TransactionID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `brand`
+--
+ALTER TABLE `brand`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `CartID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `OrderID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `order_items`
+--
+ALTER TABLE `order_items`
+  MODIFY `OrderItemsID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `profile`
+--
+ALTER TABLE `profile`
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `ReviewID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `TransactionID` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
